ea8201767cb99c7c15186ff472f817f2e517ad61
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-05-28 09:24:16 +0000

    [Add]レイアウト、バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :book, dependent: :destroy
   has_one_attached :profile_image
 
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
+  validates :self_introduction, length: { maximum: 50 }
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :create, :show, :edit, :update]
 end
