5a89177b2b3b2e790fd213a1e52f786f5d75b9e2
EC2 Default User <ec2-user@ip-172-31-30-191.ap-northeast-1.compute.internal>
2023-06-01 09:57:00 +0000

    [Fix]新規ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    user_path(resource)
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, %i(profile_image)])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, %i(profile_image)])
   end
 end
